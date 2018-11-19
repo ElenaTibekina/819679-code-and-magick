277b36fb5d62ceebaf42fbf8fe40d7dc10425638
Elena Tibekina <elentibekina@gmail.com>
2018-11-19 23:54:03 +0300

    Добавила константы, перенесла внутрь функции

--- js/stat.js
+++ js/stat.js
@@ -1,81 +1,76 @@
-"use strict";
-var currentX = 110;
-var currentY = 25;
-var y1 = 10;
-var y2 = 10;
-var step = 100;
-var cloudIndent = 10;
-var cloudBodySize = 80;
-var endY = currentY;
-var histogramHeight = 150;
-var initialX = 150;
-var initialY = 245;
-var columnIndent = 90;
-var columnWidth = 40;
-var lineHeight = 20;
+'use strict';
 
-window.renderStatistics = function(ctx, names, times) {
+window.renderStatistics = function (ctx, names, times) {
+
   // Отрисовка облака
 
-  ctx.fillStyle = "#ffffff";
+  ctx.fillStyle = '#ffffff';
   ctx.beginPath();
 
   ctx.lineTo(110, 25);
 
   ctx.shadowOffsetX = 10;
   ctx.shadowOffsetY = 10;
-  ctx.shadowColor = "rgba(0, 0, 0, 0.7)";
+  ctx.shadowColor = 'rgba(0, 0, 0, 0.7)';
 
+  var CURRENT_X = 110;
+  var CURRENT_Y = 25;
+  var y1 = 10;
+  var y2 = 10;
+  var step = 100;
+  var CLOUD_INDENT = 10;
+  var CLOUD_BODY_SIZE = 80;
+  var endY = CURRENT_Y;
   for (var i = 0; i < 4; i++) {
-    var endX = currentX + step;
-    var x1 = currentX + cloudIndent;
-    var x2 = x1 + cloudBodySize;
-    currentX += step;
+    var endX = CURRENT_X + step;
+    var x1 = CURRENT_X + CLOUD_INDENT;
+    var x2 = x1 + CLOUD_BODY_SIZE;
+    CURRENT_X += step;
     ctx.bezierCurveTo(x1, y1, x2, y2, endX, endY);
   }
 
   ctx.fill();
 
   step = 80;
-  cloudBodySize = 60;
+  CLOUD_BODY_SIZE = 60;
   x1 = 520;
   x2 = 520;
-  currentX = endX;
+  CURRENT_X = endX;
   for (var j = 0; j < 3; j++) {
-    endY = currentY + step;
-    y1 = currentY + cloudIndent;
-    y2 = y1 + cloudBodySize;
-    currentY += step;
+    endY = CURRENT_Y + step;
+    y1 = CURRENT_Y + CLOUD_INDENT;
+    y2 = y1 + CLOUD_BODY_SIZE;
+    CURRENT_Y += step;
     ctx.bezierCurveTo(x1, y1, x2, y2, endX, endY);
   }
 
   ctx.fill();
 
   step = 100;
-  cloudBodySize = 80;
+  CLOUD_BODY_SIZE = 80;
   y1 = 280;
   y2 = 280;
-  currentY = endY;
+  CURRENT_Y = endY;
   for (i = 0; i < 4; i++) {
-    endX = currentX - step;
-    x1 = currentX - cloudIndent;
-    x2 = x1 - cloudBodySize;
-    currentX -= step;
+    endX = CURRENT_X - step;
+    x1 = CURRENT_X - CLOUD_INDENT;
+    x2 = x1 - CLOUD_BODY_SIZE;
+    CURRENT_X -= step;
     ctx.bezierCurveTo(x1, y1, x2, y2, endX, endY);
   }
 
   ctx.fill();
 
   step = 80;
-  cloudBodySize = 60;
+  CLOUD_BODY_SIZE = 60;
   x1 = 100;
   x2 = 100;
-  currentX = endX;
+  CURRENT_X = endX;
   for (j = 0; j < 3; j++) {
-    endY = currentY - step;
-    y1 = currentY - cloudIndent;
-    y2 = y1 - cloudBodySize;
-    currentY -= step;
+    endY = CURRENT_Y - step;
+    y1 = CURRENT_Y - CLOUD_INDENT;
+    y2 = y1 - CLOUD_BODY_SIZE;
+    CURRENT_Y -= step;
     ctx.bezierCurveTo(x1, y1, x2, y2, endX, endY);
   }
 
@@ -85,21 +80,21 @@ window.renderStatistics = function(ctx, names, times) {
 
   ctx.shadowOffsetX = 0;
   ctx.shadowOffsetY = 0;
-  ctx.shadowColor = "transparent";
+  ctx.shadowColor = 'transparent';
 
-  ctx.fillStyle = "#000000";
-  ctx.font = "16px PT Mono";
-  ctx.fillText("Ура! Вы победили!", 130, 40);
-  ctx.fillText("Список результатов:", 130, 60);
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура! Вы победили!', 230, 40);
+  ctx.fillText('Список результатов:', 220, 60);
 
   // Вычисление максимального значения
 
-  var getMaxValue = function(anyArray) {
+  var getMaxValue = function (anyArray) {
     var maxValue = -1;
 
-    for (var i = 0; i < anyArray.length; i++) {
-      if (anyArray[i] > maxValue) {
-        maxValue = anyArray[i];
+    for (var index = 0; index < anyArray.length; index++) {
+      if (anyArray[index] > maxValue) {
+        maxValue = anyArray[index];
       }
     }
     return maxValue;
@@ -107,36 +102,34 @@ window.renderStatistics = function(ctx, names, times) {
 
   // Определение цвета
 
-  var getPlayerColor = function(playerName) {
-    if (playerName === "Вы") {
-      return "rgba(255, 0, 0, 1)";
+  var getPlayerColor = function (playerName) {
+    if (playerName === 'Вы') {
+      return 'rgba(255, 0, 0, 1)';
     } else {
-      return "rgba(0, 0, 255, " + (Math.random() + 0.1).toFixed(1) + ")";
+      return 'rgba(0, 0, 255, ' + (Math.random() + 0.1).toFixed(1) + ')';
     }
   };
 
   // Построение гистограммы
 
+  var BAR_HEIGHT = 150;
+  var INICIAL_X = 150;
+  var INICIAL_Y = 245;
+  var COLUMN_INDENT = 90;
+  var COLUMN_WIDTH = 40;
+  var LINE_HEIGHT = 20;
   var maxTime = getMaxValue(times);
 
   for (j = 0; j < times.length; j++) {
     var playerTime = Math.round(times[j]);
-    var columnHeight = (playerTime * histogramHeight) / (maxTime - 0);
+    var columnHeight = playerTime * BAR_HEIGHT / (maxTime - 0);
 
     ctx.fillStyle = getPlayerColor(names[j]);
-    ctx.fillRect(
-      initialX + j * columnIndent,
-      initialY,
-      columnWidth,
-      columnHeight * -1
-    );
+    ctx.fillRect(INICIAL_X  + j * COLUMN_INDENT, INICIAL_Y, COLUMN_WIDTH, columnHeight * (-1));
 
-    ctx.fillStyle = "#000000";
-    ctx.fillText(
-      playerTime,
-      initialX + j * columnIndent,
-      initialY - columnHeight - lineHeight / 2
-    );
-    ctx.fillText(names[j], initialX + j * columnIndent, initialY + lineHeight);
+    ctx.fillStyle = '#000000';
+    ctx.fillText(playerTime, INICIAL_X  + j * COLUMN_INDENT, INICIAL_Y - columnHeight - LINE_HEIGHT / 2);
+    ctx.fillText(names[j], INICIAL_X  + j * COLUMN_INDENT, INICIAL_Y + LINE_HEIGHT);
   }
+
 };
